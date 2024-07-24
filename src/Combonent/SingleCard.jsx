24326010aefe5b5505b66e8ce6249be96140b4cf
khalid147369 <rabaaouikhalid@gmail.com>
2024-07-24 11:52:39 +0200

    sd

--- src/Combonent/SingleCard.jsx
+++ src/Combonent/SingleCard.jsx
@@ -113,7 +113,7 @@ const SingleCard = () => {
     }
   };
   return (
-    <div className="  " >
+    <div className="  absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2  " >
       <ToastContainer />
       <div className="card   ">
       <p onClick={()=>navigate("/")} className=" absolute right-2 md:right-8 top-2 cursor-pointer text-2xl">❌</p>
